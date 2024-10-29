c2f76868923745422237a3f037879fabb54bf08c
AthichanunK <127026118+AthichanunK@users.noreply.github.com>
2024-10-30 01:46:25 +0700

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,35 @@
+<?php
+$Fname = $_POST['Fname'];
+$Lname = $_POST['Lname'];
+$Uname = $_POST['Uname'];
+$pwd = $_POST['pwd'];
+
+if (!empty($Uname) && !empty($pwd) && !empty($Fname) && !empty($Lname)) {
+    $host = "sql12.freesqldatabase.com";
+    $dbUsername = "sql12741546";
+    $dbPassword = "KAexfLWQSP";
+    $dbname = "sql12741546";
+
+    // Create connection
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Prepare an SQL statement to insert data
+    $stmt = $conn->prepare("INSERT INTO account (Fname,Lname,Uname,pwd) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $Fname, $Lname, $Uname, $pwd);
+
+    // Execute the query
+    if ($stmt->execute()) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    // Close the statement and connection
+    $stmt->close();
+    $conn->close();
+}
